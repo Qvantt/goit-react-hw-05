3839a196e16374c2c222ce790d5c082a8e7c8108
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-09-11 12:18:48 +0300

    Update MoviesPage.jsx

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,3 +1,49 @@
+// import { useState, useEffect } from "react";
+// import { getMoviesPage } from "../../service/movies-page";
+// import MovieList from "../../components/MovieList/MovieList";
+// import SearchForm from "../../components/SearchForm/SearchForm";
+// import { useLocation, useSearchParams } from "react-router-dom";
+// import Loader from "../../components/Loader/Loader";
+// import Error from "../../components/Error/Error";
+
+// export default function MoviesPage() {
+//   const [movie, setMovie] = useState([]);
+//   const [error, setError] = useState(false);
+//   const [loader, setLoader] = useState(false);
+
+//   const [params] = useSearchParams();
+
+//   const query = params.get("query");
+
+//   const location = useLocation();
+
+//   useEffect(() => {
+//     if (!query) {
+//       return;
+//     }
+//     async function getData() {
+//       setLoader(true);
+//       try {
+//         const response = await getMoviesPage(query);
+//         setMovie(response);
+//       } catch {
+//         setError(true);
+//       } finally {
+//         setLoader(false);
+//       }
+//     }
+//     getData();
+//   }, [query]);
+
+//   return (
+//     <>
+//       <SearchForm />
+//       {loader && <Loader />}
+//       {error && <Error />}
+//       {movie && <MovieList movie={movie} state={{ from: location }} />}
+//     </>
+//   );
+// }
 import { useState, useEffect } from "react";
 import { getMoviesPage } from "../../service/movies-page";
 import MovieList from "../../components/MovieList/MovieList";
@@ -11,12 +57,13 @@ export default function MoviesPage() {
   const [error, setError] = useState(false);
   const [loader, setLoader] = useState(false);
 
-  const [params] = useSearchParams();
-
-  const query = params.get("query");
+  // Використовуємо useSearchParams для управління параметрами пошуку
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query") || ""; // Отримуємо поточний запит з URL
 
   const location = useLocation();
 
+  // Викликаємо API при зміні квері
   useEffect(() => {
     if (!query) {
       return;
@@ -35,9 +82,15 @@ export default function MoviesPage() {
     getData();
   }, [query]);
 
+  // Функція handleSubmit для оновлення параметрів пошуку
+  const handleSubmit = (searchQuery) => {
+    setSearchParams({ query: searchQuery });
+  };
+
   return (
     <>
-      <SearchForm />
+      {/* Передаємо handleSubmit у форму пошуку */}
+      <SearchForm onSubmit={handleSubmit} />
       {loader && <Loader />}
       {error && <Error />}
       {movie && <MovieList movie={movie} state={{ from: location }} />}
